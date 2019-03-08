7282eb63b7aaadc163f258cd43565d942b320a77
LaVieSucree <KrugTheBearKing@gmail.com>
2019-03-07 20:32:23 -0500

    Initial Game pt 2!

---
+++ Assets/Scripts/CollectableCakes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(AudioClip))]
+public class CollectableCakes : MonoBehaviour
+{
+    public AudioClip collectableSound;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //TODO: imp
+            GetComponent<AudioSource>().PlayOneShot(collectableSound);
+            GetComponent<Renderer>().enabled = false;
+            GameObject.Destroy(gameObject, collectableSound?.length ?? 0.0f);
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(CircleCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    private Rigidbody2D _rigidBody;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        //Get the new position of our character
+        var x = transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+        var y = transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+        _rigidBody.MovePosition(new Vector2(x, y));
+    }
+}
